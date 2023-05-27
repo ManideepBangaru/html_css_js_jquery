28b3fae17435db076ddb067856184900a2a04c5d
ManideepBangaru <bmd994@gmail.com>
2023-05-27 20:38:51 +0530

    updated ternary operator

---
+++ 07TernaryOperator.js
@@ -0,0 +1,15 @@
+let num = 8
+let result
+
+// if(num%2==0){
+//     result = "Even"
+// }
+// else{
+//     result = "Odd"
+// }
+
+// console.log(result)
+
+// option 1
+result = num%2==0 ? "Even" : "Odd"
+console.log(result)
