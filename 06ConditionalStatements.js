dfd1ddbd980d727f6978cbfaae0342686b37dd76
ManideepBangaru <bmd994@gmail.com>
2023-05-27 20:21:51 +0530

    doen with conditional operators

---
+++ 06ConditionalStatements.js
@@ -0,0 +1,53 @@
+let x = 5
+let y = 4
+result = x > y
+if(result)
+    console.log("x is greater")
+console.log("Bye !!!")
+
+x = 5
+y = 4
+result = x < y
+if(result)
+    console.log("x is greater")
+else
+    console.log("y is greater")
+    console.log("yeee")
+console.log("Bye !!!")
+
+x = 5
+y = 4
+result = x > y
+if(result)
+    console.log("x is greater")
+else
+    console.log("y is greater")
+    console.log("yeee")
+console.log("Bye !!!")
+
+x = 5
+y = 4
+result = x > y
+if(result)
+    console.log("x is greater")
+else{
+    console.log("y is greater")
+    console.log("yeee")
+}
+console.log("Bye !!!")
+
+x = 5
+y = 4
+z = 8
+result = x > y
+if(result && x > z){
+    console.log("x is greater")
+}
+else if(result && x < z){
+    console.log("z is greater")
+}
+else{
+    console.log("y is greater")
+    console.log("yeee")
+}
+console.log("Bye !!!")
