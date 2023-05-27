f7d62a36a20fd39d58d34c49bccbafdb54f31d53
ManideepBangaru <bmd994@gmail.com>
2023-05-27 20:05:46 +0530

    done with conditional operators

---
+++ 05LogicalOperators.js
@@ -0,0 +1,17 @@
+let x = 7, y = 8, z = 9
+
+let result = x < y 
+
+console.log(result)
+
+result = x < y && x < z
+
+console.log(result)
+
+result = x < y && x > z
+
+console.log(result)
+
+result = x < y || x > z
+
+console.log(result)
