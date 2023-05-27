3310937206b6a81e7a8023dd7af64591daab2423
ManideepBangaru <bmd994@gmail.com>
2023-05-27 18:23:45 +0530

    completed Arithmetic operators

---
+++ 03ArithmeticOperators.js
@@ -0,0 +1,50 @@
+let num1 = 2
+let num2 = 4
+
+// Addition
+console.log(num1 + num2)
+
+// Subtraction
+console.log(num1 - num2)
+
+// Multiplication
+console.log(num1 * num2)
+
+// Division
+console.log(num1 / num2)
+
+// Remainder
+console.log(num1 % num2)
+
+// play with booleans
+let num3 = true
+let num4 = true
+
+console.log(num3 + num4)
+console.log(num3 - num4)
+console.log(num3 * num4)
+
+// different way of incrementing
+let num5 = 5
+console.log(num5)
+++num5 // pre-increment
+console.log(num5)
+num5++ // post-increment
+console.log(num5)
+
+let num6 = 7
+let x = num6++
+
+console.log(x, num6)
+
+// Math class
+let num7 = 2
+
+let result1 = num7 * num7 * num7
+console.log(result1)
+
+let result2 = Math.pow(num7, 4)
+console.log(result2)
+
+let result3 = num7 ** 2
+console.log(result3)
