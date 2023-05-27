3f414e010f6e018fcbe0a03ac86714296ef1a38b
ManideepBangaru <bmd994@gmail.com>
2023-05-27 20:00:48 +0530

    updated relational operators

---
+++ 04RelationalOperators.js
@@ -0,0 +1,18 @@
+console.log(4>5)
+console.log(4>=4)
+
+let x = "Pen"
+let y = "Book"
+console.log(x > y)
+
+y = "Pencil"
+console.log(x > y)
+
+x = "2"
+y = 1
+console.log(x > y)
+
+x = "3"
+y = 3
+console.log(x==y)
+console.log(x===y)
