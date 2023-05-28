de99763250e417bd2060db2350d0992349b7b63f
Manideep Bangaru <manideep@gameopedia.com>
2023-05-28 23:35:32 +0530

    updated function return

---
+++ 16FunctionReturnPassing.js
@@ -0,0 +1,13 @@
+function greetUser(){
+    return "Hello User"
+}
+
+let greet1 = greetUser()
+console.log(greet1)
+
+function greetUserByName(name){
+    return `Hello ${name}`
+}
+
+let greet2 = greetUserByName("Manideep Bangaru")
+console.log(greet2)
