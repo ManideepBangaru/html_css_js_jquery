769410e764cff91a8963ff017d8b288b4d5fee6d
ManideepBangaru <bmd994@gmail.com>
2023-05-28 19:46:33 +0530

    updated literals

---
+++ 09Literal.js
@@ -0,0 +1,9 @@
+let num1 = 9
+let num2 = 3
+
+let result = num1 + num2
+
+// console.log("Sum of num1 and num2 is result")
+console.log(`Sum of ${num1} and ${num2} is ${result}`)
+console.log(`My Name is
+Manideep Bangaru`)
