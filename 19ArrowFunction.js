defe487057890d48968869067f0f897018327fbb
Manideep Bangaru <manideep@gameopedia.com>
2023-05-29 00:05:11 +0530

    learned arrow function

--- 19ArrowFunction.js
+++ 19ArrowFunction.js
@@ -14,3 +14,9 @@ let sumFun = (num1, num2) => {
     return num1 + num2
 }
 console.log(sumFun(45,65))
+
+let add = (num1, num2) => num1 + num2
+console.log(add(45,65))
+
+let addPosOnly = (num1, num2) => num1 + (num2>0 ? num2 : -1*num2)
+console.log(addPosOnly(45,-65))
