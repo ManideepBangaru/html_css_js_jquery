1af2b43abb2e79fcb4b214c2b1b94db77331fd16
Manideep Bangaru <manideep@gameopedia.com>
2023-05-28 23:56:33 +0530

    learned Arrow Function

---
+++ 19ArrowFunction.js
@@ -0,0 +1,16 @@
+let greet1 = () => {
+    return 'Hello'
+}
+
+console.log(greet1())
+
+let greet2 = (user) => {
+    return `Hello ${user}`
+}
+
+console.log(greet2('Manideep'))
+
+let sumFun = (num1, num2) => {
+    return num1 + num2
+}
+console.log(sumFun(45,65))
