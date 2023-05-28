175451a659c47b60831847b86922d9cce726567c
ManideepBangaru <bmd994@gmail.com>
2023-05-28 20:12:22 +0530

    updated objects

---
+++ 12Objects.js
@@ -0,0 +1,12 @@
+let alien = {
+    name: 'Manideep',
+    tech: 'Python',
+    'work exp': '4'
+}
+
+console.log(alien)
+console.log(alien.name)
+console.log(alien["work exp"])
+
+let input = 'name'
+console.log(alien[input])
