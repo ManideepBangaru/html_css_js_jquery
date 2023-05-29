dc51768509f94e9f5c200579de33a5c657ae6068
Manideep Bangaru <manideep@gameopedia.com>
2023-05-29 18:40:53 +0530

    learned arrays

---
+++ 23CreateArray.js
@@ -0,0 +1,16 @@
+
+
+let names = ['Manideep','Manoj']
+console.log(names)
+console.log(typeof(names))
+console.log(names.length)
+
+let new_names = [];
+new_names.push('suresh')
+console.log(new_names)
+new_names.push('suresh')
+
+let values = [5, 7, 8]
+console.log(values[0])
+console.log(values[1])
+console.log(values[2])
