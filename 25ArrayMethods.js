c7491363902e5a0685fdba8337baa046531bb113
Manideep Bangaru <manideep@gameopedia.com>
2023-05-29 18:56:51 +0530

    learned array methods

---
+++ 25ArrayMethods.js
@@ -0,0 +1,24 @@
+let data = [5,7,8,9]
+console.log(data.push(2))
+console.log(data)
+
+console.log(data.pop())
+console.log(data)
+
+console.log(data.shift())
+console.log(data)
+
+console.log(data.unshift(2))
+console.log(data)
+
+let dataNew = [1,2,3,4,5,6,7,8,9]
+
+console.log(dataNew)
+console.log(dataNew.splice(3))
+console.log(dataNew)
+
+console.log(dataNew.splice(2,4))
+console.log(dataNew)
+
+console.log(dataNew.splice(1,1,4,5))
+console.log(dataNew)
