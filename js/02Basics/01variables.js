12b32953c9f7ef1f3a614ac9954764c18e1592d3
ManideepBangaru <bmd994@gmail.com>
2024-05-02 18:22:38 +0530

    started declaring variables

---
+++ js/02Basics/01variables.js
@@ -0,0 +1,15 @@
+var userName = "Manideep Bangaru"
+var userEmail = "bmd994@gmail.com"
+var isLoggedInFromGoogle = false
+var courseCount = 0
+
+const uid = 123
+console.log(uid)
+
+console.log("User Name is : " + userName)
+console.log("User Email is : " + userEmail)
+
+console.log(`
+    User Name is : ${userName}
+    And his Email ID is : ${userEmail}
+`)
